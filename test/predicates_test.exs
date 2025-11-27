76be050b57c8d54938a97d51b614650bbbc1789c
bdebinska <b.debinska@box-id.com>
2025-11-27 10:17:33 +0100

    Bump version, update docs and changelog

--- test/predicates_test.exs
+++ test/predicates_test.exs
@@ -174,7 +174,7 @@ defmodule PredicatesTest do
           )
         )
 
-    def build_assoc(:prizes, meta),
+    def build_assoc(:prizes, _opts),
       do:
         from(p in Prizes,
           where: p.author_id == parent_as(:pred_authors).id,
